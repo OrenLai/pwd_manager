9ea96f0657a1c23ba3de1a8f227b4fc235516237
OrenLai <oren0602@gmail.com>
2022-01-26 10:36:39 +0800

    add the save function and check if the entry is empty, if yes send alert or confirm with user if the data entered are correct, then write it to file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-
+from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
@@ -8,8 +8,25 @@ def generate_password():
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-def add_to_file():
-    print("add to file")
+def save():
+    site_name = site_entry.get()
+    username = username_entry.get()
+    password = password_entry.get()
+
+    if len(site_name) == 0 or len(password) == 0:
+        messagebox.showinfo(title="warning", message="site name or password can not be empty !")
+    else:
+        is_ok = messagebox.askokcancel(title="data confirm", message=f"Here are the information entered:\n "
+                                                                     f"website={site_name}\n"
+                                                                     f"password={password}\n Is it ok to save?\n")
+        if is_ok:
+            with open("data.txt", "a") as file:
+                file.write(f"{site_name} | {username} | {password}\n")
+                # print(f"{site_name} | {username} | {password}")
+            site_entry.delete(0, END)
+            password_entry.delete(0, END)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 
@@ -33,8 +50,11 @@ password_label.grid(row=3, column=0)
 
 site_entry = Entry(width=35)
 site_entry.grid(row=1, column=1, columnspan=2)
+site_entry.focus()  # let the cursor into this entry , so that user could start typing here straight away
 
 username_entry = Entry(width=35)
+# user insert to prefill the data in the entry, 0 means the start of the entry
+username_entry.insert(0, "oren@gmail.com")
 username_entry.grid(row=2, column=1, columnspan=2)
 
 password_entry = Entry(width=35)
@@ -43,7 +63,7 @@ password_entry.grid(row=3, column=1, columnspan=2)
 pwd_button = Button(text="Generate Password", command=generate_password)
 pwd_button.grid(row=4, column=1)
 
-add_button = Button(text="Add", width=32, command=add_to_file)
+add_button = Button(text="Add", width=32, command=save)
 add_button.grid(row=5, column=1, columnspan=2)
 
 window.mainloop()
