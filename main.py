cedcf33a7958028d177f2514840a7843f0db5e54
OrenLai <oren0602@gmail.com>
2022-01-27 21:26:09 +0800

    add the generate password function and use pyperclip to add the password to clipboard

--- main.py
+++ main.py
@@ -2,9 +2,31 @@ from tkinter import *
 from tkinter import messagebox
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+from random import randint, shuffle, choice
+import pyperclip
 
 def generate_password():
-    print("pwd")
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+               'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+               'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+    # randomly generate 8-10 letters , 2-4 symbols , 2-4 numbers
+    letter_list = [choice(letters) for _ in range(randint(8, 10))]
+    symbols_list = [choice(symbols) for _ in range(randint(2, 4))]
+    numbers_list = [choice(numbers) for _ in range(randint(2, 4))]
+
+    password_list = letter_list + symbols_list + numbers_list
+    # shuffle around the elements in the list
+    shuffle(password_list)
+
+    # use join method to concatenate all elements in the string
+    password = "".join(password_list)
+    password_entry.delete(0, END)
+    password_entry.insert(0, password)
+    pyperclip.copy(password)
+    print(f"Your password is: {password}")
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
@@ -60,7 +82,7 @@ username_entry.grid(row=2, column=1, columnspan=2)
 password_entry = Entry(width=35)
 password_entry.grid(row=3, column=1, columnspan=2)
 
-pwd_button = Button(text="Generate Password", command=generate_password)
+pwd_button = Button(text="Generate Password",command=generate_password)
 pwd_button.grid(row=4, column=1)
 
 add_button = Button(text="Add", width=32, command=save)
